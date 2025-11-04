518899a56bb9c448662b1c3c7e4ce3818690c937
yusufsw <yusuf-talha@outlook.com>
2025-11-05 02:03:06 +0300

    added efcore library and Sqlite and ProductsDbContext.cs -- SeedData moved to ProductsContext.cs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -12,14 +12,7 @@ namespace ProductsAPI.Controllers
 
         public ProductsController()
         {
-            _products = new List<Product>
-            {
-                new()  { ProductId = 1, ProductName = "IPhone 14", Price = 60000, IsActive = true },
-                new()  { ProductId = 2, ProductName = "IPhone 15", Price = 70000, IsActive = true },
-                new()  { ProductId = 3, ProductName = "IPhone 16", Price = 80000, IsActive = true },
-                new()  { ProductId = 4, ProductName = "IPhone 17", Price = 90000, IsActive = true },
-                new()  { ProductId = 5, ProductName = "IPhone 18", Price = 100000, IsActive = true }
-            };
+
         }
 
         //localhost:5000/api/products => GET
