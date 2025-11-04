f8b21ccc0031efe448538788f8fb1be7c759247a
yusufsw <yusuf-talha@outlook.com>
2025-11-05 01:39:16 +0300

    first commit-- ProductsController.cs

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.Models;
+
+namespace ProductsAPI.Controllers
+{
+    //localhost:5000/api/products
+    [ApiController]
+    [Route("api/[controller]")]//api/products
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product>? _products;
+
+        public ProductsController()
+        {
+            _products = new List<Product>
+            {
+                new()  { ProductId = 1, ProductName = "IPhone 14", Price = 60000, IsActive = true },
+                new()  { ProductId = 2, ProductName = "IPhone 15", Price = 70000, IsActive = true },
+                new()  { ProductId = 3, ProductName = "IPhone 16", Price = 80000, IsActive = true },
+                new()  { ProductId = 4, ProductName = "IPhone 17", Price = 90000, IsActive = true },
+                new()  { ProductId = 5, ProductName = "IPhone 18", Price = 100000, IsActive = true }
+            };
+        }
+
+        //localhost:5000/api/products => GET
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            if(_products == null)
+            {
+                return NotFound();
+            }
+            return Ok(_products);
+        }
+        
+        //localhost:5000/api/products/1 => GET
+        [HttpGet("{id}")] //{api/[controller]/{id}}
+        public IActionResult GetProduct(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound(); //StatusCode(404, "Aradiginiz Kaynak Bulunamadi");
+            }
+            var p = _products?.FirstOrDefault(i => i.ProductId == id);
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(p); //200 code yani basarili
+        }
+    }
+}
